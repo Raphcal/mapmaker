06017cb3c0bcca228a62cbc8d84a30f886f89850
daeke <rcalabro@ideia.fr>
2014-12-29 16:44:13 +0100

    Ajout du support de l'orientation horizontale à GridList.

--- src/main/java/fr/rca/mapmaker/ui/GridList.java
+++ src/main/java/fr/rca/mapmaker/ui/GridList.java
@@ -19,9 +19,75 @@ import javax.swing.JComponent;
  */
 public class GridList extends JComponent {
 	
-	private int tileSize = 6;
-	private int thumbnailSize = 72;
-	private int padding = 4;
+	public enum Orientation {
+		VERTICAL {
+			@Override
+			public Dimension getDimension(GridList list) {
+				return new Dimension(
+					list.padding + list.thumbnailSize + list.padding, 
+					list.padding + list.maps.size() * (list.thumbnailSize + list.padding));
+			}
+
+			@Override
+			public int getX(GridList list, int index) {
+				return list.padding;
+			}
+
+			@Override
+			public int getY(GridList list, int index) {
+				return list.padding + (list.thumbnailSize + list.padding) * index;
+			}
+
+			@Override
+			public int getStart(Rectangle rectangle) {
+				return rectangle.y;
+			}
+
+			@Override
+			public int getLength(Rectangle rectangle) {
+				return rectangle.height;
+			}
+		}, 
+		HORIZONTAL {
+			@Override
+			public Dimension getDimension(GridList list) {
+				return new Dimension(
+					list.padding + list.maps.size() * (list.thumbnailSize + list.padding),
+					list.padding + list.thumbnailSize + list.padding);
+			}
+			
+			@Override
+			public int getX(GridList list, int index) {
+				return list.padding + (list.thumbnailSize + list.padding) * index;
+			}
+
+			@Override
+			public int getY(GridList list, int index) {
+				return list.padding;
+			}
+			
+			@Override
+			public int getStart(Rectangle rectangle) {
+				return rectangle.x;
+			}
+
+			@Override
+			public int getLength(Rectangle rectangle) {
+				return rectangle.width;
+			}
+		};
+		
+		public abstract Dimension getDimension(GridList list);
+		public abstract int getX(GridList list, int index);
+		public abstract int getY(GridList list, int index);
+		public abstract int getStart(Rectangle rectangle);
+		public abstract int getLength(Rectangle rectangle);
+	}
+	
+	private Orientation orientation = Orientation.VERTICAL;
+	private final int tileSize = 6;
+	private final int thumbnailSize = 72;
+	private final int padding = 4;
 	private List<TileMap> maps;
 	
 	private LayerChangeListener listener;
@@ -39,8 +105,9 @@ public class GridList extends JComponent {
 			public void layerChanged(TileLayer layer, Rectangle dirtyRectangle) {
 				final Integer index = indexes.get(layer);
 				
-				if(index != null)
+				if(index != null) {
 					repaintMap(index);
+				}
 			}
 		};
 		
@@ -75,11 +142,7 @@ public class GridList extends JComponent {
 			}
 		}
 
-		final Dimension size = new Dimension(padding + thumbnailSize + padding, padding + maps.size() * (thumbnailSize + padding));
-		setPreferredSize(size);
-		setSize(size);
-		
-		invalidate();
+		updateSize();
 		
 		if(getParent() != null) {
 			getParent().validate();
@@ -88,13 +151,25 @@ public class GridList extends JComponent {
 		repaint();
 	}
 
+	private void updateSize() {
+		final Dimension size = orientation.getDimension(this);
+		setPreferredSize(size);
+		setSize(size);
+		
+		invalidate();
+	}
+
 	public List<TileMap> getMaps() {
 		return maps;
+	}
+
+	public void setOrientation(Orientation orientation) {
+		this.orientation = orientation;
+		updateSize();
 	}
 	
 	@Override
 	protected void paintComponent(Graphics g) {
-		
 		final Rectangle clipBounds = g.getClipBounds();
 		
 		if(isOpaque()) {
@@ -102,26 +177,27 @@ public class GridList extends JComponent {
 			g.fillRect(clipBounds.x, clipBounds.y, clipBounds.width, clipBounds.height);
 		}
 		
-		final int topMap = (clipBounds.y - padding) / (thumbnailSize + padding);
-		final int bottomMap = topMap + clipBounds.height / (thumbnailSize + padding);
+		final int firstMap = (orientation.getStart(clipBounds) - padding) / (thumbnailSize + padding);
+		final int lastMap = firstMap + orientation.getLength(clipBounds) / (thumbnailSize + padding);
 		
-		for(int index = topMap; index <= bottomMap; index++)
+		for(int index = firstMap; index <= lastMap; index++) {
 			paintMap(index, g);
-			
+		}
 		
 		g.dispose();
 	}
 	
 	private void paintMap(int index, Graphics g) {
-		
-		if(index < 0 || index >= maps.size())
+		if(index < 0 || index >= maps.size()) {
 			return;
+		}
 		
 		final TileMap map = maps.get(index);
 		final Palette palette = map.getPalette();
 		
 		final int size = thumbnailSize / tileSize;
-		final int originY = padding + index * (thumbnailSize + padding);
+		final int originX = orientation.getX(this, index);
+		final int originY = orientation.getY(this, index);
 		
 		if(map.getBackgroundColor() != null) {
 			g.setColor(map.getBackgroundColor());
@@ -133,15 +209,16 @@ public class GridList extends JComponent {
 				final int maxX = Math.min(size, layer.getWidth());
 				final int maxY = Math.min(size, layer.getHeight());
 				
-				for(int y = 0; y < maxY; y++)
-					for(int x = 0; x < maxX; x++)
-						palette.paintTile(g, layer.getTile(x, y), padding + x * tileSize, originY + y * tileSize, tileSize);
+				for(int y = 0; y < maxY; y++) {
+					for(int x = 0; x < maxX; x++) {
+						palette.paintTile(g, layer.getTile(x, y), originX + x * tileSize, originY + y * tileSize, tileSize);
+					}
+				}
 			}
 		}
 	}
 	
 	private void repaintMap(int index) {
-		
 		repaint(new Rectangle(0, padding + index * (thumbnailSize + padding), thumbnailSize + padding, thumbnailSize + padding));
 	}
 }
