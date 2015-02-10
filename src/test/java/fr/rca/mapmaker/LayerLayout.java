10962b63ea9e6785bbcae9943bd96871ec918d88
daeke <rcalabro@ideia.fr>
2015-02-10 10:36:49 +0100

    Début d'un layout pour superposer des composants au centre ou dans un coin.

---
+++ src/test/java/fr/rca/mapmaker/LayerLayout.java
@@ -0,0 +1,105 @@
+package fr.rca.mapmaker;
+
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
+import java.awt.Insets;
+import java.awt.LayoutManager;
+import java.awt.Rectangle;
+
+/**
+ *
+ * @author Raphaël Calabro (rcalabro@ideia.fr)
+ */
+public class LayerLayout implements LayoutManager {
+	
+	public static enum Disposition {
+		TOP_LEFT {
+
+			@Override
+			public Rectangle getBounds(Container parent, Component component) {
+				final Dimension preferredSize = component.getPreferredSize();
+				return new Rectangle(preferredSize.width, preferredSize.height);
+			}
+			
+		},
+		CENTER {
+
+			@Override
+			public Rectangle getBounds(Container parent, Component component) {
+				final Dimension size = parent.getSize();
+				final Dimension preferredSize = component.getPreferredSize();
+				
+				return new Rectangle(
+					(size.width - preferredSize.width)/2,
+					(size.height - preferredSize.height)/2, 
+					preferredSize.width, preferredSize.height);
+			}
+			
+		};
+		
+		public abstract Rectangle getBounds(Container parent, Component component);
+	}
+	
+	private Disposition disposition;
+	
+	public LayerLayout() {
+		disposition = Disposition.CENTER;
+	}
+
+	public LayerLayout(Disposition disposition) {
+		this.disposition = disposition;
+	}
+
+	@Override
+	public void addLayoutComponent(String name, Component comp) {
+	}
+
+	@Override
+	public void removeLayoutComponent(Component comp) {
+	}
+
+	@Override
+	public Dimension preferredLayoutSize(Container parent) {
+		final Dimension size = new Dimension(0, 0);
+		
+		for(final Component component : parent.getComponents()) {
+			final Dimension preferredSize = component.getPreferredSize();
+			
+			size.width = Math.max(preferredSize.width, size.width);
+			size.height = Math.max(preferredSize.height, size.height);
+		}
+		
+		final Insets insets = parent.getInsets();
+		size.width += insets.left + insets.right;
+		size.height += insets.top + insets.bottom;
+		
+		return size;
+	}
+
+	@Override
+	public Dimension minimumLayoutSize(Container parent) {
+		final Dimension size = new Dimension(0, 0);
+		
+		for(final Component component : parent.getComponents()) {
+			final Dimension preferredSize = component.getMinimumSize();
+			
+			size.width = Math.max(preferredSize.width, size.width);
+			size.height = Math.max(preferredSize.height, size.height);
+		}
+		
+		final Insets insets = parent.getInsets();
+		size.width += insets.left + insets.right;
+		size.height += insets.top + insets.bottom;
+		
+		return size;
+	}
+
+	@Override
+	public void layoutContainer(Container parent) {
+		for(final Component component : parent.getComponents()) {
+			component.setBounds(disposition.getBounds(parent, component));
+		}
+	}
+	
+}
---
+++ src/test/java/fr/rca/mapmaker/LayoutPreview.java
@@ -0,0 +1,38 @@
+package fr.rca.mapmaker;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author Raphaël Calabro (rcalabro@ideia.fr)
+ */
+public class LayoutPreview {
+	public static void main(String[] args) {
+		final JFrame frame = new JFrame();
+
+		final JPanel red = new JPanel();
+		red.setPreferredSize(new Dimension(50, 50));
+//		red.setOpaque(false);
+		red.setBackground(new Color(1.0f, 0.0f, 0.0f, 0.5f));
+
+		final JPanel blue = new JPanel();
+		blue.setPreferredSize(new Dimension(50, 50));
+//		green.setOpaque(false);
+		blue.setBackground(new Color(0.0f, 0.0f, 1.0f, 0.5f));
+		
+		final JPanel gridBag = new JPanel(new LayerLayout(LayerLayout.Disposition.TOP_LEFT));
+		gridBag.setOpaque(true);
+		gridBag.setPreferredSize(new Dimension(150, 150));
+		gridBag.add(red);
+		gridBag.add(blue);
+		
+		frame.setContentPane(gridBag);
+		
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.pack();
+		frame.setVisible(true);
+	}
+}
