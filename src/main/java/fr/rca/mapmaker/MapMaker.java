01dbdaa0b8d44640f88c0ec3bc71d69bc098c655
daeke <rcalabro@ideia.fr>
2015-02-19 12:51:16 +0100

    Repassage à Java 1.6 pour permettre l'encapsulation dans un bundle Mac OS X + Mise à jour de l'icône.

--- src/main/java/fr/rca/mapmaker/MapMaker.java
+++ src/main/java/fr/rca/mapmaker/MapMaker.java
@@ -2,20 +2,17 @@ package fr.rca.mapmaker;
 
 import fr.rca.mapmaker.editor.MapEditor;
 import fr.rca.mapmaker.exception.Exceptions;
-import java.io.IOException;
 
 import java.awt.EventQueue;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.prefs.BackingStoreException;
 import java.util.prefs.Preferences;
 import javax.imageio.ImageIO;
 import javax.swing.JFrame;
 import javax.swing.UIManager;
-import javax.swing.UnsupportedLookAndFeelException;
 
 public class MapMaker {
 	public static final String PREFERENCES_WIDTH = "width";
@@ -34,45 +31,49 @@ public class MapMaker {
 		
 		try {
 			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
-		} catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException ex) {
+		} catch (/* ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeel */ Exception ex) {
 			// Look & Feel système non disponible, ignoré.
 		}
 		
 		final Preferences preferences = Preferences.userNodeForPackage(MapMaker.class);
 		
-		EventQueue.invokeLater(() -> {
-			final MapEditor editor = new MapEditor();
-			editor.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-			
-			editor.addWindowListener(new WindowAdapter() {
-				@Override
-				public void windowClosed(WindowEvent e) {
-					preferences.putInt(PREFERENCES_WIDTH, editor.getWidth());
-					preferences.putInt(PREFERENCES_HEIGHT, editor.getHeight());
-					preferences.putInt(PREFERENCES_EXTENDED_STATE, editor.getExtendedState());
-					
-					try {
-						preferences.sync();
-					} catch (BackingStoreException ex) {
-						Exceptions.showStackTrace(ex, editor);
+		EventQueue.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				final MapEditor editor = new MapEditor();
+				editor.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+				editor.addWindowListener(new WindowAdapter() {
+					@Override
+					public void windowClosed(WindowEvent e) {
+						preferences.putInt(PREFERENCES_WIDTH, editor.getWidth());
+						preferences.putInt(PREFERENCES_HEIGHT, editor.getHeight());
+						preferences.putInt(PREFERENCES_EXTENDED_STATE, editor.getExtendedState());
+
+						try {
+							preferences.sync();
+						} catch (BackingStoreException ex) {
+							Exceptions.showStackTrace(ex, editor);
+						}
+
+						System.exit(0);
 					}
-					
-					System.exit(0);
-				}
-			});
-			
-			final int width = preferences.getInt(PREFERENCES_WIDTH, editor.getWidth());
-			final int height = preferences.getInt(PREFERENCES_HEIGHT, editor.getHeight());
-			final int extendedState = preferences.getInt(PREFERENCES_EXTENDED_STATE, editor.getExtendedState());
-			
-			editor.setSize(width, height);
-			editor.setExtendedState(extendedState);
-			editor.setVisible(true);
-			
-			if(args.length == 1) {
-				final File file = new File(args[0]);
-				if(file.exists()) {
-					editor.openFile(file);
+				});
+
+				final int width = preferences.getInt(PREFERENCES_WIDTH, editor.getWidth());
+				final int height = preferences.getInt(PREFERENCES_HEIGHT, editor.getHeight());
+				final int extendedState = preferences.getInt(PREFERENCES_EXTENDED_STATE, editor.getExtendedState());
+
+				editor.setSize(width, height);
+				editor.setExtendedState(extendedState);
+				editor.setVisible(true);
+
+				if(args.length == 1) {
+					final File file = new File(args[0]);
+					if(file.exists()) {
+						editor.openFile(file);
+					}
 				}
 			}
 		});
@@ -89,7 +90,7 @@ public class MapMaker {
 				setDockIconImageMethod.invoke(application, ImageIO.read(MapMaker.class.getResourceAsStream("/resources/icon.png")));
 			}
 			
-		} catch (ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | IOException ex) {
+		} catch (/* ClassNotFoundException | NoSuchMethodException | SecurityException | IllegalAccessException | IllegalArgumentException | InvocationTargetException | IO */ Exception ex) {
 			// Ignoré.
 		}
 	}
