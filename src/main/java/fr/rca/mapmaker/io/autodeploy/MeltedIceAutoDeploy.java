8e4af7f1f14a781fa9b63dac7e972b8555bafc0c
daeke <rcalabro@ideia.fr>
2015-06-23 12:37:13 +0200

    Mise à jour du fichier projet de Visual Studio lors du déploiement auto.

--- src/main/java/fr/rca/mapmaker/io/autodeploy/MeltedIceAutoDeploy.java
+++ src/main/java/fr/rca/mapmaker/io/autodeploy/MeltedIceAutoDeploy.java
@@ -20,6 +20,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Comparator;
 import java.util.List;
+import javax.xml.stream.XMLStreamException;
 
 /**
  *
@@ -64,9 +65,17 @@ public class MeltedIceAutoDeploy {
 		
 		FORMAT.setVersion(InternalFormat.LAST_VERSION);
 		
-		deploySprites(project.getSprites(), spritesFolder);
-		deployPalettes(project.getPalettes(), mapsFolder);
-		deployMaps(project.getMaps(), project, mapsFolder);
+		final List<String> contents = new ArrayList<String>();
+		
+		deploySprites(project.getSprites(), spritesFolder, contents);
+		deployPalettes(project.getPalettes(), mapsFolder, contents);
+		deployMaps(project.getMaps(), project, mapsFolder, contents);
+		
+		try {
+			VSProjectWriter.write(project, root, contents, new FileOutputStream(new File(root, "MeltedIce.csproj")));
+		} catch (XMLStreamException ex) {
+			throw new IOException("Erreur lors de l'écriture du fichier de projet VisualStudio.", ex);
+		}
 	}
 	
 	public static boolean accept(File file) {
@@ -76,15 +85,17 @@ public class MeltedIceAutoDeploy {
 		return file.isDirectory() && mapsFolder.isDirectory() && spritesFolder.isDirectory();
 	}
 	
-	private static void deploySprites(Collection<Sprite> sprites, File folder) throws IOException {
+	private static void deploySprites(Collection<Sprite> sprites, File folder, List<String> files) throws IOException {
 		final PackMap packMap = PackMap.packSprites(sprites, 1);
 		if(packMap != null) {
 			// Image
 			final DataHandler<BufferedImage> imageDataHandler = FORMAT.getHandler(BufferedImage.class);
 			
-			final FileOutputStream imageOutputStream = new FileOutputStream(new File(folder, fr.rca.mapmaker.io.mkz.ProjectDataHandler.IMAGE_FILE_NAME));
+			final File imageFile = new File(folder, fr.rca.mapmaker.io.mkz.ProjectDataHandler.IMAGE_FILE_NAME);
+			final FileOutputStream imageOutputStream = new FileOutputStream(imageFile);
 			try {
 				imageDataHandler.write(packMap.renderImage(), imageOutputStream);
+				addFile(imageFile, files);
 			} finally {
 				imageOutputStream.close();
 			}
@@ -92,52 +103,53 @@ public class MeltedIceAutoDeploy {
 			// Atlas
 			final DataHandler<PackMap> packMapDataHandler = FORMAT.getHandler(PackMap.class);
 			
-			final FileOutputStream dataOutputStream = new FileOutputStream(new File(folder, fr.rca.mapmaker.io.mkz.ProjectDataHandler.DATA_FILE_NAME));
+			final File dataFile = new File(folder, fr.rca.mapmaker.io.mkz.ProjectDataHandler.DATA_FILE_NAME);
+			final FileOutputStream dataOutputStream = new FileOutputStream(dataFile);
 			try {
 				packMapDataHandler.write(packMap, dataOutputStream);
+				addFile(dataFile, files);
 			} finally {
 				dataOutputStream.close();
 			}
 		}
 	}
 	
-	private static void deployPalettes(List<Palette> palettes, File folder) throws IOException {
+	private static void deployPalettes(List<Palette> palettes, File folder, List<String> files) throws IOException {
 		final DataHandler<Palette> paletteDataHandler = FORMAT.getHandler(Palette.class);
 		final DataHandler<BufferedImage> imageHandler = FORMAT.getHandler(BufferedImage.class);
 		
-		for(int index = 0; index < palettes.size(); index++) {
-			final String name = index < PALETTE_NAMES.length ? PALETTE_NAMES[index] : null;
-			if(name != null) {
-				final Palette palette = palettes.get(index);
+		for (Palette palette : palettes) {
+			final File paletteFile = new File(folder, palette.toString() + ".pal");
+			final FileOutputStream paletteOutputStream = new FileOutputStream(paletteFile);
+			try {
+				paletteDataHandler.write(palette, paletteOutputStream);
+				addFile(paletteFile, files);
+			} finally {
+				paletteOutputStream.close();
+			}
 
-				final FileOutputStream paletteOutputStream = new FileOutputStream(new File(folder, name));
-				try {
-					paletteDataHandler.write(palette, paletteOutputStream);
-				} finally {
-					paletteOutputStream.close();
-				}
-
-				// Écriture des images.
-				final BufferedImage image = fr.rca.mapmaker.io.mkz.ProjectDataHandler.renderPalette(palette, palette.getTileSize());
-				final FileOutputStream imageOutputStream = new FileOutputStream(new File(folder, palette.toString() + '-' + palette.getTileSize() + ".png"));
-				try {
-					imageHandler.write(image, imageOutputStream);
-				} finally {
-					imageOutputStream.close();
-				}
+			// Écriture des images.
+			final File imageFile = new File(folder, palette.toString() + ".png");
+			final BufferedImage image = fr.rca.mapmaker.io.mkz.ProjectDataHandler.renderPalette(palette, palette.getTileSize());
+			final FileOutputStream imageOutputStream = new FileOutputStream(imageFile);
+			try {
+				imageHandler.write(image, imageOutputStream);
+				addFile(imageFile, files);
+			} finally {
+				imageOutputStream.close();
 			}
 		}
 	}
 	
-	private static void deployMaps(List<TileMap> maps, Project project, File folder) throws IOException {
+	private static void deployMaps(List<TileMap> maps, Project project, File folder, List<String> files) throws IOException {
 		final DataHandler<TileMap> tileMapHandler = FORMAT.getHandler(TileMap.class);
 		final DataHandler<Instance> instanceHandler = FORMAT.getHandler(Instance.class);
 		
 		for(int index = 0; index < maps.size(); index++) {
-			final String baseName = index < MAP_NAMES.length ? MAP_NAMES[index] : null;
+			final TileMap map = maps.get(index);
+			final String baseName = getBaseName(map);
 			
 			if(baseName != null) {
-				final TileMap map = maps.get(index);
 				final List<Instance> instances = new ArrayList<Instance>(project.getAllInstances().get(index));
 				instances.sort(new Comparator<Instance>() {
 
@@ -154,20 +166,24 @@ public class MeltedIceAutoDeploy {
 
 
 				// Écriture de la carte
-				final FileOutputStream mapOutputStream = new FileOutputStream(new File(folder, baseName + ".map"));
+				final File mapFile = new File(folder, baseName + ".map");
+				final FileOutputStream mapOutputStream = new FileOutputStream(mapFile);
 				try {
 					tileMapHandler.write(map, mapOutputStream);
+					addFile(mapFile, files);
 				} finally {
 					mapOutputStream.close();
 				}
 
 				// Écriture des instances
-				final FileOutputStream instancesOutputStream = new FileOutputStream(new File(folder, baseName + ".sprites"));
+				final File instancesFile = new File(folder, baseName + ".sprites");
+				final FileOutputStream instancesOutputStream = new FileOutputStream(instancesFile);
 				try {
 					Streams.write(instances.size(), instancesOutputStream);
 					for(final Instance instance : instances) {
 						instanceHandler.write(instance, instancesOutputStream);
 					}
+					addFile(instancesFile, files);
 				} finally {
 					instancesOutputStream.close();
 				}
@@ -183,5 +199,19 @@ public class MeltedIceAutoDeploy {
 		} else {
 			throw new UnsupportedOperationException("Format non reconnu.");
 		}
+	}
+	
+	private static void addFile(File file, List<String> files) {
+		files.add(file.getParentFile().getName() + '\\' + file.getName());
+	}
+	
+	private static String getBaseName(TileMap map) {
+		final String name = map.getName();
+		
+		if(name == null) {
+			return name;
+		}
+		
+		return name.toLowerCase().replace(' ', '-');
 	}
 }
--- src/main/java/fr/rca/mapmaker/io/autodeploy/VSProjectWriter.java
+++ src/main/java/fr/rca/mapmaker/io/autodeploy/VSProjectWriter.java
@@ -1,9 +1,165 @@
 package fr.rca.mapmaker.io.autodeploy;
 
+import fr.rca.mapmaker.model.project.Project;
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import javax.xml.stream.XMLOutputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.XMLStreamWriter;
+
 /**
  *
  * @author Raphaël Calabro <raph_kun at yahoo.fr>
  */
 public class VSProjectWriter {
+	public static void write(Project project, File projectRoot, List<String> contents, OutputStream outputStream) throws XMLStreamException {
+		final XMLOutputFactory factory = XMLOutputFactory.newFactory();
+		final XMLStreamWriter writer = factory.createXMLStreamWriter(outputStream);
+		
+		writer.writeStartDocument();
+		
+		writer.writeStartElement("Project");
+		writer.writeAttribute("DefaultTargets", "Build");
+		writer.writeAttribute("ToolsVersion", "4.0");
+		writer.writeDefaultNamespace("http://schemas.microsoft.com/developer/msbuild/2003");
+		
+		writer.writeStartElement("PropertyGroup");
+		writeElement(writer, "Configuration", " '$(Configuration)' == '' ", "Debug");
+		writeElement(writer, "Platform", " '$(Platform)' == '' ", "AnyCPU");
+		writeElement(writer, "ProductVersion", "10.0.0");
+		writeElement(writer, "SchemaVersion", "2.0");
+		writeElement(writer, "ProjectGuid", "{753347C6-8677-4F27-8346-D60E5E79A76F}");
+		writeElement(writer, "ProjectTypeGuids", "{69878862-DA7D-4DC6-B0A1-50D8FAB4242F};{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}");
+		writeElement(writer, "OutputType", "Exe");
+		writeElement(writer, "RootNamespace", "MeltedIce");
+		writeElement(writer, "AssemblyName", "MeltedIce");
+		writer.writeEndElement();
+		
+		writeCompileMode(writer, "Debug", true, "full");
+		writeCompileMode(writer, "Release", false, "none");
+		
+		writeItemGroup(writer, "Reference", "System", "System.Xml", "System.Core", "Sce.PlayStation.Core");
+		writeItemGroup(writer, "Compile", getSources(projectRoot));
+		writeItemGroup(writer, "ShaderProgram", getShaders(projectRoot));
+		writeItemGroup(writer, "PsmMetadata", "app.xml");
+		
+		writer.writeEmptyElement("Import");
+		writer.writeAttribute("Project", "$(MSBuildExtensionsPath)\\Sce\\Sce.Psm.CSharp.targets");
+		
+		writeItemGroup(writer, "Folder", "maps\\", "sprites\\", "audio\\", "scripts\\");
+		
+		final List<String> allContents = new ArrayList<String>(contents);
+		allContents.addAll(Arrays.asList(getFileNames(projectRoot, "audio", ".wav", ".mp3")));
+		allContents.addAll(Arrays.asList(getFileNames(projectRoot, "scripts", ".lua")));
+		writeItemGroup(writer, "Content", allContents.toArray(new String[allContents.size()]));
+		
+		writer.writeStartElement("ItemGroup");
+		writeProjectReference(writer, "LuaInterface", "..\\..\\LuaInterface\\LuaInterface\\LuaInterface.csproj", "{D29CA5AA-9C3A-47FD-A145-20A13F147E83}");
+		writer.writeEndElement();
+		
+		writer.writeEndElement();
+	}
 	
+	private static void writeCompileMode(XMLStreamWriter writer, String mode, boolean debugSymbols, String debugType) throws XMLStreamException {
+		writeStartElement(writer, "PropertyGroup", " '$(Configuration)|$(Platform)' == '" + mode + "|AnyCPU' ");
+		
+		if(debugSymbols) {
+			writeElement(writer, "DebugSymbols", debugSymbols);
+		}
+		writeElement(writer, "DebugType", debugType);
+		writeElement(writer, "Optimize", true);
+		writeElement(writer, "OutputPath", "bin\\" + mode);
+		
+		if(debugSymbols) {
+			writeElement(writer, "DefineConstants", "DEBUG;");
+		}
+		writeElement(writer, "ErrorReport", "prompt");
+		writeElement(writer, "WarningLevel", 4);
+		writeElement(writer, "ConsolePause", false);
+		
+		writer.writeEndElement();
+	}
+	
+	private static void writeItemGroup(XMLStreamWriter writer, String category, String... includes) throws XMLStreamException {
+		writer.writeStartElement("ItemGroup");
+		
+		for(final String include : includes) {
+			writer.writeEmptyElement(category);
+			writer.writeAttribute("Include", include);
+		}
+		
+		writer.writeEndElement();
+	}
+	
+	private static void writeElement(XMLStreamWriter writer, String element, String value) throws XMLStreamException {
+		writer.writeStartElement(element);
+		writer.writeCharacters(value);
+		writer.writeEndElement();
+	}
+	
+	private static void writeElement(XMLStreamWriter writer, String element, int value) throws XMLStreamException {
+		writeElement(writer, element, Integer.toString(value));
+	}
+	
+	private static void writeElement(XMLStreamWriter writer, String element, boolean value) throws XMLStreamException {
+		writeElement(writer, element, Boolean.toString(value));
+	}
+	
+	private static void writeStartElement(XMLStreamWriter writer, String element, String condition) throws XMLStreamException {
+		writer.writeStartElement(element);
+		writer.writeAttribute("Condition", condition);
+	}
+	
+	private static void writeElement(XMLStreamWriter writer, String element, String condition, String value) throws XMLStreamException {
+		writeStartElement(writer, element, condition);
+		writer.writeCharacters(value);
+		writer.writeEndElement();
+	}
+	
+	private static void writeProjectReference(XMLStreamWriter writer, String name, String include, String uid) throws XMLStreamException {
+		writer.writeStartElement("ProjectReference");
+		writer.writeAttribute("Include", include);
+		
+		writeElement(writer, "Project", uid);
+		writeElement(writer, "Name", name);
+		
+		writer.writeEndElement();
+	}
+	
+	private static String[] getSources(File projectRoot) {
+		return getFileNames(projectRoot, null, ".cs");
+	}
+	
+	private static String[] getShaders(File projectRoot) {
+		return getFileNames(projectRoot, "shaders", ".fcg", ".vcg");
+	}
+	
+	private static String[] getFileNames(File folder, String subFolder, final String... extensions) {
+		final File root = subFolder != null ? new File(folder, subFolder) : folder;
+		
+		final String[] files = root.list(new FilenameFilter() {
+
+			@Override
+			public boolean accept(File dir, String name) {
+				for(final String extension : extensions) {
+					if(name.endsWith(extension)) {
+						return true;
+					}
+				}
+				return false;
+			}
+		});
+		
+		if(subFolder != null) {
+			for(int index = 0; index < files.length; index++) {
+				files[index] = subFolder + '\\' + files[index];
+			}
+		}
+		
+		return files;
+	}
 }
