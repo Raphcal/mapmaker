a7c5a8e601a509ae1f77cbdb243bd91e78d08902
daeke <daeke@192.168.106.219>
2016-03-03 16:33:59 +0100

    Ajout de javadoc.

--- src/main/java/fr/rca/mapmaker/model/map/LayerPlugins.java
+++ src/main/java/fr/rca/mapmaker/model/map/LayerPlugins.java
@@ -1,11 +1,19 @@
 package fr.rca.mapmaker.model.map;
 
 /**
- *
+ * Ensemble de méthodes utilitaires pour gérer les plugins de layers.
+ * 
  * @author Raphaël Calabro (raphael.calabro@netapsys.fr)
  */
 public final class LayerPlugins {
 	
+	/**
+	 * Copie le plugin donné si différent de <code>null</code>.
+	 * 
+	 * @param plugin Plugin à copier.
+	 * @return Une copie du plugin ou <code>null</code> si le plugin donné
+	 * est <code>null</code>.
+	 */
 	public static LayerPlugin copyOf(LayerPlugin plugin) {
 		if (plugin != null) {
 			return plugin.copy();
