51765a918833e5e3ea9e6d6ceffb54d9fe23d611
Raphaël Calabro <ddaeke-github@yahoo.fr>
2023-01-18 21:50:41 +0100

    Ajout d'un séparateur pour permettre d'appliquer une fonction différente pour hitbox et déformation

--- src/main/java/fr/rca/mapmaker/editor/tool/ApplyFunctionTool.java
+++ src/main/java/fr/rca/mapmaker/editor/tool/ApplyFunctionTool.java
@@ -12,20 +12,31 @@ import javax.swing.JOptionPane;
  * @author Raphaël Calabro (raphael.calabro@netapsys.fr)
  */
 public class ApplyFunctionTool {
+	private static final char SEPARATOR = ';';
 
 	public static void execute(DataLayer layer) {
-		final String function = JOptionPane.showInputDialog("Fonction à appliquer (exemple : \"x * 2 + zoom(12)\") ?");
+		final String function = JOptionPane.showInputDialog("Fonction à appliquer (exemple : \"x * 2 + zoom(12)" + SEPARATOR + "+32\") ?");
 		execute(layer, function);
 	}
 
 	public static void execute(DataLayer layer, String function) {
+		final int separatorIndex = function.lastIndexOf(SEPARATOR);
+		final String suffix;
+		if (separatorIndex >= 0) {
+			suffix = function.substring(separatorIndex + 1);
+			// Retire le suffixe de la fonction pour appliquer juste une partie de la fonction à la déformation.
+			function = function.substring(0, separatorIndex);
+		} else {
+			suffix = "";
+		}
+
 		final Operation operation = OperationParser.parse(function);
-		
+
 		final int[] source = layer.copyData();
 		final int[] destination = new int[source.length];
 		final int width = layer.getWidth();
 		final int height = layer.getHeight();
-		
+
 		for (int x = 0; x < width; x++) {
 			final int top = (int) operation.execute(x);
 			for (int y = 0; y < top && y < height; y++) {
@@ -40,11 +51,11 @@ public class ApplyFunctionTool {
 				}
 			}
 		}
-		
+
 		if (layer instanceof HasLayerPlugin) {
-			((HasLayerPlugin) layer).setPlugin(new FunctionLayerPlugin(function));
+			((HasLayerPlugin) layer).setPlugin(new FunctionLayerPlugin(function + suffix));
 		}
 		layer.restoreData(destination, null);
 	}
-	
+
 }
